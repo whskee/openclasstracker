5bab91f5832be9bfe1bc4333ac69ef91428f495b
Chinmaya Bidkar <chinmayabidkar@hotmail.com>
2021-06-12 23:06:15 -0700

    Reformat fix space

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name="core"
-
+app_name = "core"
 urlpatterns = [
     path("", views.index, name="index"),
     path("about/", views.about, name="about"),
